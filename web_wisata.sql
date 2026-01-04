d7eff1c525d4c7e6166bebeb3557054bed227847
dafasshh <daffaramadhan661@gmail.com>
2026-01-04 22:28:34 +0700

    Update data database terbaru

--- web_wisata.sql
+++ web_wisata.sql
@@ -1,27 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 04, 2026 at 10:44 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- BAGIAN PERBAIKAN: Membuat dan Memilih Database Otomatis
+CREATE DATABASE IF NOT EXISTS web_wisata;
+USE web_wisata;
 
+-- KODE ASLI (Lanjutan):
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `web_wisata`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `destinasi`
